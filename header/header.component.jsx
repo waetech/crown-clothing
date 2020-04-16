d37da03408e489e2898f49efcd0604da2abd7a02
Roxanne Reyes <34749680+waetech@users.noreply.github.com>
2020-04-15 22:08:10 -0400

    Integrated firebase into application and sign in
    Created sign in, updated header, form input, custom button components and styles.

---
+++ header/header.component.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils';
+
+import './header.styles.scss';
+
+
+const Header = ({ currentUser }) => (
+    <div className='header'>
+       <Link to="/">
+        <Logo className='logo'/>
+       </Link>
+        <div className='options'>
+            <Link className='option'>
+                SHOP
+            </Link>
+                <Link className='option'>
+                  CONTACT
+                </Link>
+                {
+                    currentUser ?
+                    <div className='option' onClick={() => auth.signOut()}>
+                        SIGN OUT
+                        </div>
+                    :
+                    <Link className='option' to='/signin'>
+                        SIGN IN
+                    </Link>
+                }
+        </div>
+        
+    </div>
+);
+
+export default Header;
